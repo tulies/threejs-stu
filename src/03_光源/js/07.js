ffa711404451816e6f1146f630d9c5073bcde455
王嘉炀 <346461062@qq.com>
2019-12-09 18:53:46 +0800

    update

--- src/03_光源/js/07.js
+++ src/03_光源/js/07.js
@@ -25,15 +25,14 @@ class ThreeCase {
       // color: 0xFFFFFF
       map: textureGrass
     });
-    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.receiveShadow = true;
-    plane.position.set(15, 0, 0);
-    plane.rotation.x = -0.5 * Math.PI;
-    this.scene.add(plane);
-
+    this.plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    this.plane.receiveShadow = true;
+    this.plane.position.set(15, 0, 0);
+    this.plane.rotation.x = -0.5 * Math.PI;
+    this.scene.add(this.plane);
 
     // 添加2个几何体
-    const cubeAndSphere = addDefaultCubeAndSphere(scene);
+    const cubeAndSphere = addDefaultCubeAndSphere(this.scene);
     const cube = cubeAndSphere.cube;
     cube.geometry.faces.forEach(function (face) {
       face.color.copy(new THREE.Color(Math.random() * 0xffffff));
@@ -42,10 +41,10 @@ class ThreeCase {
     const sphere = cubeAndSphere.sphere;
     // const plane = addGroundPlane(scene);
 
+
+
   }
   initLight(){
-
-
     // 一点点光
     const spotLight0 = new THREE.SpotLight(0xcccccc);
     spotLight0.position.set(-40, 60, -10);
@@ -54,70 +53,43 @@ class ThreeCase {
     this.scene.add(spotLight0);
 
 
+    var pointColor = "#ffffff";
+    //    var spotLight = new THREE.SpotLight( pointColor);
+    var spotLight = new THREE.DirectionalLight(pointColor);
+    spotLight.position.set(30, 10, -50);
+    spotLight.intensity = 1;
+    spotLight.castShadow = true;
+    spotLight.shadowCameraNear = 0.1;
+    spotLight.shadowCameraFar = 100;
+    spotLight.shadowCameraFov = 50;
+    spotLight.target = this.plane;
+    spotLight.distance = 0;
+    spotLight.shadowCameraNear = 2;
+    spotLight.shadowCameraFar = 200;
+    spotLight.shadowCameraLeft = -100;
+    spotLight.shadowCameraRight = 100;
+    spotLight.shadowCameraTop = 100;
+    spotLight.shadowCameraBottom = -100;
+    spotLight.shadowMapWidth = 2048;
+    spotLight.shadowMapHeight = 2048;
+    this.spotLight = spotLight;
+    this.scene.add(this.spotLight);
 
-    // 这个不知道干嘛用的，没看到起到啥作用
-    THREE.RectAreaLightUniformsLib.init();
+  }
+    // 镜头眩晕的代码,需要额外导入js文件 /libs/three/objects/Lensflare.js
 
-    this.areaLight1 = new THREE.RectAreaLight(0xff0000, 500, 4, 10);
-    this.areaLight1.position.copy(this.plane1.position);
-    this.areaLight1.lookAt(this.plane1.position.x, this.plane1.position.y, this.plane1.position.z+1);
-    this.scene.add(this.areaLight1);
+  initLensFlare(){
+    const textureFlare0 = THREE.ImageUtils.loadTexture("../../assets/textures/flares/lensflare0.png");
+    // const textureFlare3 = THREE.ImageUtils.loadTexture("../../assets/textures/flares/lensflare3.png");
 
-    // 这面这段是官方的demo写法
-    // var rectLightMesh = new THREE.Mesh( new THREE.PlaneBufferGeometry(), new THREE.MeshBasicMaterial( {  color: 0xff0000 } ) );
-    // rectLightMesh.scale.x = this.areaLight1.width;
-    // rectLightMesh.scale.y = this.areaLight1.height;
-    // this.areaLight1.add( rectLightMesh );
-    // var rectLightMeshBack = new THREE.Mesh( new THREE.PlaneBufferGeometry(), new THREE.MeshBasicMaterial( { color: 0x080808 } ) );
-    // rectLightMesh.add( rectLightMeshBack );
+    const flareColor = new THREE.Color(0xffaacc);
+    const lensFlare = new THREE.LensFlare(textureFlare0, 350, 0.0, THREE.AdditiveBlending, flareColor);
+    lensFlare.position.copy(this.spotLight.position);
+    this.scene.add(lensFlare);
 
 
-    // 新版本RectAreaLightHelper好像没啥作用，位置固定再原点。。
-    // const rectLightHelper = new THREE.RectAreaLightHelper( this.areaLight1 );
-    // this.scene.add(rectLightHelper);
-
-    this.areaLight2 = new THREE.RectAreaLight(0x00ff00, 500, 4, 10);
-    this.areaLight2.position.copy(this.plane2.position);
-    this.areaLight2.lookAt(this.plane2.position.x, this.plane2.position.y, this.plane2.position.z+1);
-
-    this.scene.add(this.areaLight2);
-
-    this.areaLight3 = new THREE.RectAreaLight(0x0000ff, 500, 4, 10);
-    this.areaLight3.position.copy(this.plane3.position);
-    this.areaLight3.lookAt(this.plane3.position.x, this.plane3.position.y, this.plane3.position.z+1);
-    this.scene.add(this.areaLight3);
   }
   initGUIControls(){
-    const controls = {
-      color1:this.areaLight1.color.getHex(),
-      intensity1:this.areaLight1.intensity,
-      color2:this.areaLight2.color.getHex(),
-      intensity2:this.areaLight2.intensity,
-      color3:this.areaLight3.color.getHex(),
-      intensity3:this.areaLight3.intensity,
-    };
-    const gui = new dat.GUI();
-    gui.addColor(controls, 'color1').onChange(e=>{
-      this.areaLight1.color = new THREE.Color(e);
-      this.plane1.material.color.set(new THREE.Color(e));
-    });
-    gui.add(controls, 'intensity1', 0, 1000).onChange(e=>{
-      this.areaLight1.intensity = e;
-    });
-    gui.addColor(controls, 'color2').onChange(e=>{
-      this.areaLight2.color = new THREE.Color(e);
-      this.plane2.material.color.set(new THREE.Color(e));
-    });
-    gui.add(controls, 'intensity2', 0, 1000).onChange(e=>{
-      this.areaLight2.intensity = e;
-    });
-    gui.addColor(controls, 'color3').onChange(e=>{
-      this.areaLight3.color = new THREE.Color(e);
-      this.plane3.material.color.set(new THREE.Color(e));
-    });
-    gui.add(controls, 'intensity3', 0, 1000).onChange(e=>{
-      this.areaLight3.intensity = e;
-    });
 
   }
   render(){
